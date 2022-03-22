455ee8b82bdb15ccf6a083fcb384ae34d4f0236f
Danol <czdanol@gmail.com>
2022-03-22 18:48:11 +0100

    Initial commit

---
+++ Loupedeck_DNLMIDIPlugin/Loupedeck_DNLMIDIApplication.cs
@@ -0,0 +1,15 @@
+namespace Loupedeck.Loupedeck_DNLMIDIPlugin
+{
+	using System;
+
+	public class Loupedeck_DNLMIDIApplication : ClientApplication
+	{
+		public Loupedeck_DNLMIDIApplication() {
+
+		}
+
+		protected override String GetProcessName() => "";
+
+		protected override String GetBundleName() => "";
+	}
+}
---
+++ Loupedeck_DNLMIDIPlugin/Loupedeck_DNLMIDIPlugin.cs
@@ -0,0 +1,15 @@
+namespace Loupedeck.Loupedeck_DNLMIDIPlugin
+{
+	using System;
+
+	public class Loupedeck_DNLMIDIPlugin : Plugin
+	{
+
+
+		public override void RunCommand(String commandName, String parameter) {
+		}
+
+		public override void ApplyAdjustment(String adjustmentName, String parameter, Int32 diff) {
+		}
+	}
+}
