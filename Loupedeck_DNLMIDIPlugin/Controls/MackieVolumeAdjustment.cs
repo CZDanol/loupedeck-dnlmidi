7aa5b71982de64ce63e578098b5582f188bfa1a0
Danol <czdanol@gmail.com>
2022-03-22 22:19:21 +0100

    no message

--- Loupedeck_DNLMIDIPlugin/Controls/MackieVolumeAdjustment.cs
+++ Loupedeck_DNLMIDIPlugin/Controls/MackieVolumeAdjustment.cs
@@ -12,14 +12,21 @@ namespace Loupedeck.Loupedeck_DNLMIDIPlugin.Controls
 	{
 		private Loupedeck_DNLMIDIPlugin plugin = null;
 
-
-		public MackieVolumeAdjustment() : base("_", "_", "_", true) {
-			plugin = base.Plugin as Loupedeck_DNLMIDIPlugin;
-
+		public MackieVolumeAdjustment() : base(true) {
 			this.Description = "Mackie Control compatible channel fader";
 
 			for (int i = 0; i < Loupedeck_DNLMIDIPlugin.ChannelCount; i++)
-				AddParameter(i.ToString(), $"Channel {i + 1} volume", "Mackie Volume");
+				AddParameter(i.ToString(), $"CH{i + 1} volume", "Mackie Volume");
+		}
+
+		protected override bool OnLoad() {
+			plugin = base.Plugin as Loupedeck_DNLMIDIPlugin;
+
+			plugin.ChannelDataChanged += (object sender, ChannelData cd) => {
+				ActionImageChanged(cd.ChannelID.ToString());
+			};
+
+			return true;
 		}
 
 		protected override void ApplyAdjustment(string actionParameter, int diff) {
@@ -27,18 +34,25 @@ namespace Loupedeck.Loupedeck_DNLMIDIPlugin.Controls
 				return;
 
 			ChannelData cd = plugin.channelData[actionParameter];
-			cd.Volume = Math.Min(255, Math.Max(0, cd.Volume + diff));
+			cd.Volume = Math.Min(127, Math.Max(0, cd.Volume + diff));
 
-			plugin.midiOut.SendEvent(new ControlChangeEvent((SevenBitNumber)0, (SevenBitNumber)cd.Volume));
+			var e = new ControlChangeEvent();
+			e.ControlNumber = (SevenBitNumber)7; // Volume
+			e.ControlValue = (SevenBitNumber)cd.Volume;
+			e.Channel = (FourBitNumber)cd.ChannelID;
+			plugin.midiOut.SendEvent(e);
 
 			ActionImageChanged(actionParameter);
 		}
 
 		protected override BitmapImage GetCommandImage(string actionParameter, PluginImageSize imageSize) {
+			if (actionParameter == null)
+				return null;
+
 			ChannelData cd = plugin.channelData[actionParameter];
 
 			var bb = new BitmapBuilder(imageSize);
-			bb.DrawText($"Channel {cd.ChannelID + 1}\n\u00A0\n{((float)cd.Volume) / 255} %"); // NBSP on the middle line to prevent coalescing
+			bb.DrawText($"Channel {cd.ChannelID + 1}\n{Math.Round(cd.Volume / 127.0f * 100.0f)} %");
 			return bb.ToImage();
 		}
 
--- Loupedeck_DNLMIDIPlugin/Loupedeck_DNLMIDIPlugin.cs
+++ Loupedeck_DNLMIDIPlugin/Loupedeck_DNLMIDIPlugin.cs
@@ -3,6 +3,7 @@ namespace Loupedeck.Loupedeck_DNLMIDIPlugin
 	using System;
 	using System.Collections.Generic;
 	using System.Threading;
+	using Melanchall.DryWetMidi.Core;
 	using Melanchall.DryWetMidi.Multimedia;
 
 	public class Loupedeck_DNLMIDIPlugin : Loupedeck.Plugin
@@ -17,12 +18,16 @@ namespace Loupedeck.Loupedeck_DNLMIDIPlugin
 		public IDictionary<string, ChannelData> channelData = new Dictionary<string, ChannelData>();
 
 		string midiInName, midiOutName;
-		
+
+		public event EventHandler<ChannelData> ChannelDataChanged;
+
 		public string MidiInName {
 			get => midiInName;
 			set {
 				midiInName = value;
 				midiIn = InputDevice.GetByName(value);
+				midiIn.EventReceived += OnMidiEvent;
+				midiIn.StartEventsListening();
 				SetPluginSetting("midiIn", value);
 			}
 		}
@@ -39,7 +44,9 @@ namespace Loupedeck.Loupedeck_DNLMIDIPlugin
 		public Loupedeck_DNLMIDIPlugin() {
 			for (int i = 0; i < 16; i++)
 				channelData[i.ToString()] = new ChannelData(i);
+		}
 
+		public override void Load() {
 			if (TryGetPluginSetting("midiIn", out midiInName))
 				MidiInName = midiInName;
 
@@ -62,6 +69,24 @@ namespace Loupedeck.Loupedeck_DNLMIDIPlugin
 		}
 
 		public override void ApplyAdjustment(String adjustmentName, String parameter, Int32 diff) {
+		}
+
+		private void OnMidiEvent(object sender, MidiEventReceivedEventArgs args) {
+			MidiEvent e = args.Event;
+			if (e is ChannelEvent) {
+				if (!channelData.TryGetValue(((int)(e as ChannelEvent).Channel).ToString(), out ChannelData cd))
+					return;
+
+				if (e is ControlChangeEvent) {
+					var ce = e as ControlChangeEvent;
+
+					// Volume
+					if (ce.ControlNumber == 7) {
+						cd.Volume = ce.ControlValue;
+						ChannelDataChanged.Invoke(this, cd);
+					}
+				}
+			}
 		}
 
 	}
